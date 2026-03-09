1d37ee75b24f913b53189ae7c3dd9bcdd778b5dd
Pragyavish <pragyavishwakarma209@gmail.com>
2026-03-09 22:59:24 +0530

    Changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -152,6 +152,35 @@ exports.deleteUser = async (req, res) => {
   }
 };
 
+// ✅ Activate / Deactivate User
+exports.toggleUserStatus = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    // Toggle status
+    user.status = user.status === "Active" ? "Inactive" : "Active";
+
+    await user.save();
+
+    return res.status(200).json({
+      message: `User ${user.status === "Active" ? "Activated" : "Deactivated"} successfully`,
+      status: user.status
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error"
+    });
+  }
+};
+
 
 
 // ✅ Change Password (Without bcrypt)
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,9 +8,14 @@ const {
   updateUser,
   deleteUser,
   changePassword
+
 } = require("../controllers/userController");
 
 const { authenticate } = require("../middleware/auth.middleware");
+const { toggleUserStatus } = require("../controllers/userController");
+
+router.patch("/toggle-status/:id", authenticate, toggleUserStatus);
+
 
 // Protected Routes
 
@@ -25,5 +30,6 @@ router.put("/update-user/:id", authenticate, updateUser);
 router.delete("/delete-user/:id", authenticate, deleteUser);
 
 router.patch("/change-password/:id", authenticate, changePassword);
+
 
 module.exports = router;
